40d3c0eb15d6668cf5d2996ef6cd3b04e6a43fb7
René Warren <rwarren@bcgsc.ca>
2017-12-19 14:01:06 -0800

    removed hard-coded paths

--- RAILS_v1.2/runRAILS.sh
+++ RAILS_v1.2/runRAILS.sh
@@ -15,8 +15,8 @@ cat $2 | perl -ne 'if(/^\>/){  }else{my $len=length($_);my @scaftigs=split(/N+/i
 echo Building sequence database index out of your $1-formatted.fa assembly contigs..
 bwa index $1-formatted.fa
 echo Aligning long sequences $2-scaftigformatted.fa to your contigs..
-#bwa mem -x intractg -a -t24 $1-formatted.fa $2-formatted.fa  | /gsc/btl/linuxbrew/bin/samtools view -Sb - > $2_vs_$1_gapfilling.bam
-bwa mem -a -t24 $1-formatted.fa $2-scaftigformatted.fa  | /gsc/btl/linuxbrew/bin/samtools view -Sb - > $2_vs_$1_gapfilling.bam
+#bwa mem -x intractg -a -t24 $1-formatted.fa $2-formatted.fa  | samtools view -Sb - > $2_vs_$1_gapfilling.bam
+bwa mem -a -t24 $1-formatted.fa $2-scaftigformatted.fa  | samtools view -Sb - > $2_vs_$1_gapfilling.bam
 echo Scaffolding $1-formatted.fa using $2-scaftigformatted.fa and filling gaps with sequences in $2-formatted.fa
 ./cobbler.pl -f $1 -s $2_vs_$1_gapfilling.bam -d $3 -i $4 -b $2_vs_$1_$3_gapsFill -q $2-scaftigformatted.fa
 echo Process terminated.
@@ -26,7 +26,7 @@ cat $2_vs_$1_$3_gapsFill.fa | perl -ne 'if(/^\>/){$ct++;}else{my $len=length($_)
 echo Building sequence database index out of your $2_vs_$1_$3_gapsFill-formatted.fa assembly contigs..
 bwa index $2_vs_$1_$3_gapsFill-formatted.fa
 echo Aligning long sequences $2-formatted.fa to your contigs..
-bwa mem -a -t24 $2_vs_$1_$3_gapsFill-formatted.fa $2-formatted.fa | /gsc/btl/linuxbrew/bin/samtools view -Sb - > $2_vs_$1_scaffolding.bam
+bwa mem -a -t24 $2_vs_$1_$3_gapsFill-formatted.fa $2-formatted.fa | samtools view -Sb - > $2_vs_$1_scaffolding.bam
 echo Scaffolding $2_vs_$1_$3_gapsFill-formatted.fa using $2-formatted.fa and filling new gaps with sequences in $2-formatted.fa
 ./RAILS -f $2_vs_$1_$3_gapsFill-formatted.fa -s $2_vs_$1_scaffolding.bam -d $3 -i $4 -b $2_vs_$1_$3_rails -q $2-formatted.fa
 echo RAILS process terminated.
