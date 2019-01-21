760a548a0c59854ffd0ccbd38b5dba2e1a7ac1ec
Rene <rwarren@bcgsc.ca>
2019-01-21 15:24:49 -0800

    update data path ftp to http

--- versions/RAILS_v1.4.1/test/runmeHuman.sh
+++ versions/RAILS_v1.4.1/test/runmeHuman.sh
@@ -1,8 +1,8 @@
 ##Example of automated finishing (cobbler) and scaffolding (RAILS) done on HG004 genome in a bottle human genome assembly
 #Downloading the draft assembly
-wget ftp://ftp.bcgsc.ca/supplementary/RAILS/hsapiens-8.fa.gz
+wget http://www.bcgsc.ca/downloads/supplementary/RAILS/hsapiens-8.fa.gz
 #Dowloading the simulated long sequences, derived from the human genome reference (1, 2.5, 5, 15kbp reads)
-wget ftp://ftp.bcgsc.ca/supplementary/RAILS/reads.fa.gz
+wget http://www.bcgsc.ca/downloads/supplementary/RAILS/reads.fa.gz
 #Decompressing files
 gunzip hsapiens-8.fa.gz
 gunzip reads.fa.gz
